51c80bb8d34010464583343e5e1be1f9176a1493
gabrielsan <gabrielssanches@gmail.com>
2020-04-09 17:46:31 +0200

    Calls /odrive/calibrate_motors service whenever odrive status reports it is connected

--- src/odrv_monitor/src/monitor/odrv_monitor.py
+++ src/odrv_monitor/src/monitor/odrv_monitor.py
@@ -3,16 +3,32 @@
 
 import rospy
 from std_msgs.msg import String
+import std_srvs.srv
 
 def odrv_status_cb(data):
     rospy.loginfo(rospy.get_caller_id() + " ODrive status received: %s", data.data)
+    calibrate_handle = rospy.ServiceProxy('/odrive/calibrate_motors', std_srvs.srv.Trigger)
+
+    if (data.data == "connected"):
+        rospy.loginfo("calling calibration")
+        res = calibrate_handle()
+        rospy.loginfo("calibrate res: %d %s", res.success, res.message)
+    else:
+        rospy.loginfo("nothing to do")
 
 def monitor():
     #
+    rospy.loginfo("Waiting for /odrive/calibrate_motors service to be available")
+    rospy.wait_for_service('/odrive/calibrate_motors')
+    rospy.loginfo("/odrive/calibrate_motors service is available now")
+
+    rospy.loginfo("Starting node")
     rospy.init_node("odrv_monitor", anonymous=True)
 
+    rospy.loginfo("Subscribing to topic /odrive/status")
     rospy.Subscriber("/odrive/status", String, odrv_status_cb)
 
+    rospy.loginfo("Waiting for status messages from now on")
     rospy.spin()
 
 if __name__ == '__main__':
